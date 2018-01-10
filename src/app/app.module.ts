e193cf281a426825fab6f3ff8166d768747cb2f0
Fernandez de Loaysa Guillermo <Guillermo.FernandezdeLoaysa@novatec-gmbh.de>
2018-01-10 11:30:33 +0100

    Setup material & routing. First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule} from "./material.module";
+
 
 
 import { AppComponent } from './app.component';
 
+const appRoutes: Routes = [
+  /* { path: "crisis-center", component: CrisisListComponent },
+  { path: "hero/:id", component: HeroDetailComponent },
+  {
+    path: "heroes",
+    component: HeroListComponent,
+    data: { title: "Heroes List" }
+  },
+  {
+    path: "",
+    redirectTo: "/heroes",
+    pathMatch: "full"
+  },
+  { path: "**", component: PageNotFoundComponent } */
+];
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

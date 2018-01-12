950be1867500eefed0bc303454aa47f5b8c8500d
Fernandez de Loaysa Guillermo <Guillermo.FernandezdeLoaysa@novatec-gmbh.de>
2018-01-12 12:47:12 +0100

    make spreadsheets module accesible

--- src/app/score-tracker/score-table/score-table.component.ts
+++ src/app/score-tracker/score-table/score-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { GoogleDriveProvider } from "../spreadsheet.module";
+import { GoogleDriveProvider } from "../../spreadsheet.module";
 
 import { MatTableDataSource, MatSort } from "@angular/material";
 

64ddda88c9e892f59d55172c43ee3cfd8ad019c0
Guillermo Loaysa <gloaysa@gmail.com>
2018-01-16 19:39:01 +0100

    update player model

--- src/app/models/playersData.ts
+++ src/app/models/playersData.ts
@@ -1,12 +1,12 @@
 export class playersData {
     position: number;
-    player: string;
+    name: string;
     points: number;
     winner: boolean;
     playing: boolean;
     constructor(pos, name, points) {
         this.position = pos;
-        this.player = name;
+        this.name = name;
         this.points = points;
         this.winner = false;
         this.playing = false;

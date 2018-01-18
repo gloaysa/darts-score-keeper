7479eb911245cd03c3a8ed0ab7a10de18200da44
Fernandez de Loaysa Guillermo <Guillermo.FernandezdeLoaysa@novatec-gmbh.de>
2018-01-18 14:27:50 +0100

    Update how to display and interchange player1 and player2 in view

--- src/app/game-view/game-view.component.ts
+++ src/app/game-view/game-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { PlayersDataService } from "../../shared/playersdata.service";
+import { Player } from "../../models/player.model";
 
 @Component({
   selector: "app-game-view",
@@ -8,18 +9,32 @@ import { PlayersDataService } from "../../shared/playersdata.service";
   providers: []
 })
 export class GameViewComponent {
-  public player1;
-  public player2;
+  private playersData: Array<Player>;
+  public player1: Player;
+  public player2: Player;
 
   changePlayers() {
-    const player1 = this.player1;
-    this.player1 = this.player2;
-    this.player2 = player1;
+
+    this.playersData.forEach(player => {
+      if (player.player1) {
+        player.player1 = !player.player1;
+        player.player2 = !player.player2;
+        this.playersService.selectPlayers(undefined, player);
+
+      } else if (player.player2) {
+        player.player2 = !player.player2;
+        player.player1 = !player.player1;
+        this.playersService.selectPlayers(player, undefined);
+
+      }
+    });
+    this.playersService.updatePlayersData(this.playersData);
   }
 
   constructor(private playersService: PlayersDataService) {}
 
   ngOnInit() {
+    this.playersService.sharePlayersData$.subscribe(data => this.playersData = data);
     this.playersService.sharePlayer1$.subscribe(player => this.player1 = player);
     this.playersService.sharePlayer2$.subscribe(player => this.player2 = player);
   }

e21969262f687642b7756a5156cae09a07239597
Guillermo Loaysa <gloaysa@gmail.com>
2018-03-04 19:47:34 +0100

    Move circle-tracker to player (this time for real)

--- src/app/game-view/player/player.module.ts
+++ src/app/game-view/player/player.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CircleTrackerComponent } from '../../player/game-view/circle-tracker/circle-tracker.component';
+import { CircleTrackerComponent } from './circle-tracker/circle-tracker.component';
 import { SharedModule } from '../../shared/shared.module';
 import { PlayerComponent } from './player.component';
 

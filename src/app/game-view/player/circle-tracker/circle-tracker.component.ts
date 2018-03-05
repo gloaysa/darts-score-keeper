b2a44cbb599224da425f69a8213eeb39b2937b16
Guillermo Loaysa <gloaysa@gmail.com>
2018-03-05 21:02:03 +0100

    Refactor and disable circle when both are closed

--- src/app/game-view/player/circle-tracker/circle-tracker.component.ts
+++ src/app/game-view/player/circle-tracker/circle-tracker.component.ts
@@ -12,41 +12,7 @@ export class CircleTrackerComponent implements OnChanges {
 
   @Input() player1: Player;
   @Input() player2: Player;
-
-  private addPoints(player: Player, index) {
-    const circle = player.circles[index];
-    const enemy: Player = player === this.player1 ? this.player2 : this.player1;
-    const enemyCircle: Circle = enemy.circles[index];
-
-    if (circle.closed && !enemyCircle.closed) { player.points = player.points + circle.number; }
-    this.changeCircleStatus(circle, player, index);
-  }
-
-  private changeCircleStatus(circle, player, index) {
-    if (circle.once && circle.twice && !circle.closed) { player.circles[index].closed = true; }
-    if (circle.once && !circle.twice) { player.circles[index].twice = true; }
-    if (!circle.once) { player.circles[index].once = true; }
-  }
-  private selectState(player, index) {
-    if (player.circles[index].closed) { return "closed"; }
-    if (player.circles[index].twice) { return "twice"; }
-    if (player.circles[index].once) { return "once"; }
-  }
-
-  private selectPlayer(player) {
-    if (this.player1 === player) { return this.player1 }
-    if (this.player2 === player) { return this.player2 }
-  }
-
-  private createCircles(player: Player) {
-    if (player !== undefined) {
-      for (let i = 15; i < 22; i++) {
-        const circle = new Circle(i, 0);
-        player.circles.push(circle);
-      }
-    }
-    
-  }
+  circlesLeft: number = 7;
 
   ngOnChanges(change) {
     if (change.player1) {
@@ -56,4 +22,55 @@ export class CircleTrackerComponent implements OnChanges {
       this.createCircles(this.selectPlayer(change.player2.currentValue));
     }
   }
+
+  private createCircles(player: Player) {
+    if (player !== undefined) {
+      for (let i = 15; i < 22; i++) {
+        const circle = new Circle(i, 0);
+        player.circles.push(circle);
+      }
+    }
+  }
+  private addPoints(player: Player, index) {
+    const circle = player.circles[index];
+    const enemy: Player = player === this.player1 ? this.player2 : this.player1;
+    const enemyCircle: Circle = enemy.circles[index];
+
+    if (circle.closed && !enemyCircle.closed) { player.points = player.points + circle.number; }
+    this.changeCircleStatus(circle, player);
+    if (this.bothCirclesClosed(circle, enemyCircle)) {
+      this.reduceCirclesLeft();
+      this.disableCircle(circle, enemyCircle);
+    };
+  }
+
+  private changeCircleStatus(circle: Circle, player: Player) {
+    if (circle.once && circle.twice && !circle.closed) { circle.closed = true; }
+    if (circle.once && !circle.twice) { circle.twice = true; }
+    if (!circle.once) { circle.once = true; }
+  }
+  private selectState(player: Player, index) {
+    if (player.circles[index].closed) { return "closed"; }
+    if (player.circles[index].twice) { return "twice"; }
+    if (player.circles[index].once) { return "once"; }
+  }
+
+  private selectPlayer(player: Player) {
+    if (this.player1 === player) { return this.player1 }
+    if (this.player2 === player) { return this.player2 }
+  }
+
+  private reduceCirclesLeft() {
+    this.circlesLeft--;
+  }
+
+  private bothCirclesClosed(circle1: Circle, circle2: Circle) {
+    return circle1.closed && circle2.closed ? true : false;
+  }
+
+  private disableCircle(circle1: Circle, circle2: Circle) {
+    circle1.disabled = true;
+    circle2.disabled = true;
+  }
+
 }
--- src/app/models/circle.model.ts
+++ src/app/models/circle.model.ts
@@ -3,12 +3,14 @@ export class Circle {
     once: boolean;
     twice: boolean;
     closed: boolean;
+    disabled: boolean;
     player: number;
     constructor(number, player) {
         this.number = number;
         this.once = false;
         this.twice = false;
         this.closed = false;
+        this.disabled = false;
         this.player = player;
     }
 }
